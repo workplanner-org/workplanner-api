e04e913947f971882b1d8878d98bc16116c18b2c
nabil amimer <amimernabil@gmail.com>
2022-08-30 21:26:07 -0400

    added cors

--- server/workplanner/src/main/java/com/workplanner/controller/EmployeeController.java
+++ server/workplanner/src/main/java/com/workplanner/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.workplanner.model.Employee.EmployeeRepository;
 
 @Controller
 @RequestMapping(path = "/employees")
+// @CrossOrigin(origins = "http://localhost:8080")
 public class EmployeeController {
     
     @Autowired
